a581579919c3aa61146943f7502d24f8a29ebb74
Ver-Luca <verteramoluca@gmail.com>
2025-05-23 12:59:37 +0200

    PathCreatoR rewrite to create genesets ready for clusterProfiler enricher() and GSEA() functions

--- R/PathCreatoR.R
+++ R/PathCreatoR.R
@@ -1,20 +1,30 @@
-PathCreatoR <- function(organism, subcollection){
-  if (organism == "hs"){
-    if (!(subcollection %in% names(gsea_human))) {
-      stop("Subcollection not found. Please select one of: ", paste(names(gsea_human), collapse = ", "))
-    }
-    return (gsea_human[[subcollection]])
-  }
-
-  else if (organism == "mm"){
-    if (!(subcollection %in% names(gsea_mouse))) {
-      stop("Subcollection not found. Please select one of: ", paste(names(gsea_mouse), collapse = "\n"))
-    }
-    return (gsea_mouse[[subcollection]])
-  }
-
-  else{
+PathCreatoR <- function(organism, subcollection, as.df = FALSE) {
+  # Check if organism is valid
+  if (!(organism %in% c("hs", "mm"))) {
     stop("Organism not found. Please select one of: hs, mm")
   }
-}
 
+  # Check if subcollection is valid
+  if (organism == "hs") {
+    gsea_data <- gsea_human
+  } else {
+    gsea_data <- gsea_mouse
+  }
+  if (!(subcollection %in% names(gsea_data))) {
+    stop("Subcollection not found. Please select one of: ", paste(names(gsea_data), collapse = ", "))
+  }
+
+  # Extract pathway data
+  pathway_data <- gsea_data[[subcollection]]
+
+  # Convert to data frame if requested
+  if (as.df) {
+    pathway_df <- do.call(rbind, lapply(names(pathway_data), function(term) {
+      data.frame(TERM = term, Gene = pathway_data[[term]], stringsAsFactors = FALSE)
+    }))
+    return(pathway_df)
+  }
+
+  # Return pathway data as list
+  return(pathway_data)
+}
